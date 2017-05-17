75af96e61f3bd1dcef182a56af8aa762658fd4a4
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-05-17 12:46:27 +0100

    Added first test for number 1

---
+++ FizzBuzzRunner.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FizzBuzz 
+{
+    static class FizzBuzzRunner 
+    {
+        static public int Next(int ordinal)
+        {
+            return ordinal;
+        }
+    }
+}
--- FizzBuzzTests.cs
+++ FizzBuzzTests.cs
@@ -1,10 +1,17 @@
 using System;
 using Xunit;
+using FluentAssertions;
 
 namespace FizzBuzz
 {
     public class FizzBuzzTests
     {
-        
+        [Fact]
+        public void Should_print_number_if_not_3_or_5() 
+        {
+            var result = FizzBuzzRunner.Next(1);
+
+            result.Should().Be(1);
+        }
     }
 }
